0efbb855fb787331e75ad229f3c833f33ae8e456
Ford Berry <fordaberry@gmail.com>
2024-08-29 12:58:52 -0600

    created code that plays a single round of the game

--- javascript.js
+++ javascript.js
@@ -1,6 +1,11 @@
 // Step 1: Setup the project structure
 console.log("Hello world!")
 
+// Step 4: Declare the players score variables
+// Global variables
+let humanScore = 0;
+let computerScore = 0;
+
 // Step 2: Write the logic to get the computer choice
 // Generate a random number between 0 and 2
 function getComputerChoice() {
@@ -26,6 +31,38 @@ function getHumanChoice() {
 }
 
 console.log(getHumanChoice());
+
+// Step 5: Write the logic to play a single round
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+        console.log("It's a tie!");  
+    
+    } else if (humanChoice == "scissors" && computerChoice == "paper") {
+        console.log("You Win! Scissors beats paper");
+        humanScore = humanScore + 1;
+    } else if (humanChoice == "rock" && computerChoice == "scissors") {
+        console.log("You Win! Rock beats paper");
+        humanScore = humanScore + 1;
+    } else if (humanChoice == "paper" && computerChoice == "rock") {
+        console.log("You win! Paper beats rock");
+        humanScore = humanScore + 1;
+    } else if (humanChoice == "scissors" && computerChoice == "rock") {
+        console.log("You lose! Rock beats scissors");
+        computerScore = computerScore + 1;
+    } else if (humanChoice == "rock" && computerChoice == "paper") {
+        console.log("You lose! Paper beats rock!");
+        computerScore = computerScore + 1;  
+    } else if (humanChoice == "paper" && computerChoice == "scissors") {
+        console.log("You lose! Scissors beats paper!");
+        computerScore = computerScore + 1;
+    }
+        
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection)
 
 
 
